db17d5c9139d4c8f91dfb81e1aa8456412811ae7
SvetlanaSeldusheva <svetasten4ina@gmail.com>
2024-09-16 17:45:52 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,5 @@
+a = home_lessons = "12"
+b = home_hours = "1.5"
+kyrs_name = "Python"
+c = time_for_one_home_lesson = float (b) / int (a)
+print('Курс:',kyrs_name,', всего задач:',home_lessons,', затрачено часов:',home_hours,', среднее время выполнения:',c)
