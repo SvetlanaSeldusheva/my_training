2f460667d7529ef4ceb32fe591c3a8f8b668b020
SvetlanaSeldusheva <svetasten4ina@gmail.com>
2024-09-19 08:53:53 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = 1,2,7,False, "life"
+print(immutable_var)
+
+mutable_list = ["sun", "life", "smile", 51]
+print(mutable_list)
+mutable_list [3] = "happy"
+print(mutable_list)
+mutable_list.extend("heart")
+print(mutable_list)
