760ea86ea683b416f9f52881af3e02b6fd559677
SvetlanaSeldusheva <svetasten4ina@gmail.com>
2024-09-22 15:25:14 +0300

    Add files via upload

---
+++ modul_1_6.py
@@ -0,0 +1,6 @@
+m = my_dict = {'Svetlana': 1984, 'Kostya': 2008, 'Stepan': 2012}
+print(m)
+print(m['Svetlana'])
+print(m.get('Sasha'))
+my_dict.update({'Slava': 1987,'Kirill': 2023})
+print(my_dict)
